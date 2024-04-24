91ea937d32a10c8414a4e128ae6338ca862c0e26
Konrad <konra.kondracki@wp.pl>
2024-04-24 15:49:32 +0200

    poprawa linkow

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_04_17_125359) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "branzas", force: :cascade do |t|
     t.string "nazwa", null: false
     t.datetime "created_at", null: false
